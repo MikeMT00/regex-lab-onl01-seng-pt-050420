d4761a95f2601cdbf87029d1e81f6f7dfee1c1b2
Michael Thomas <michaelthomas0611@gmail.com>
2020-05-19 14:15:17 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,9 +11,19 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z].+[\.!?]$/)
+     then
+      return true
+   else
+       return false
+   end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/)
+    then
+     return true
+  else
+     return false
+  end
 end
